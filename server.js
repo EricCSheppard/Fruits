deb4ba6ae9a9d86410e389baaf8def609695337f
EricCSheppard <EricCSheppard@gmail.com>
2023-01-17 11:52:56 -0500

    Adds fruit route

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express') // import the express framework
 const mongoose = require('mongoose') // import the mongoose library
 const morgan = require('morgan') // import the morgan request logger
 require('dotenv').config()  // load my ENV file's variables
+const path = require('path') // import path module
+const { ppid } = require('process')
 
+// Import Our Models ---------------------------
+const Fruit = require('./models/fruit')
 
 // Database Connection ---------------------------
 // this is where we will set up our inputs for our datacase connect function
@@ -26,11 +30,47 @@ mongoose.connection
 // Create our Express App Object ----------------------
 const app = express()
 
+// Middleware ---------------------------------
+// middleware runs before all the routes
+// every request is processed through our middlewear before mongoose can do anything with it
+app.use(morgan('tiny')) // this is for request logging, the 'tiny' argument declares what size of morgan log to use
+app.use(express.urlencoded({ extended: true })) //this parses urlEncoded request bodies (useful for POST and PUT request)
+app.use(express.static('public')) // this serves static files from the 'public' folder
+app.use(express.json()) // parses incoming request payloads with JSON
 
 
+// Routes ------------------------------------
+app.get('/', (req, res) => {
+    res.send('Server is live, ready for requests')
+})
 
 
-
+// we're going to build a seed route
+// this will seed the database for us with a few started resources
+// there are two ways we will talk about seeding the database
+// First -> seed route, they work but they are not best practices
+// Second -> seed script, they work and they are best practices
+app.get('/fruits/seed', (req, res) => {
+    // array of starter resources (fruits)
+    const startFruits = [
+        { name: 'Orange', color: 'orange', readyToEat: true },
+        { name: 'Grape', color: 'purple', readyToEat: true  },
+        { name: 'Banana', color: 'yellow', readyToEat: false },
+        { name: 'Strawberry', color: 'red', readyToEat: false },
+        { name: 'Coconut', color: 'brown', readyToEat: true }
+    ]
+    // then we delete every fruit in the database(all instances of this resource)
+    Fruit.deleteMany({})
+        .then(() => {
+            //then we'll seed (create) our starter fruits
+            Fruit.create(startFruits)
+                // tell our db what to do with success and failures
+                .then(data => {
+                    res.json(data)
+                })
+                .catch(err => console.log('the following error occurredL \n', err))
+        })
+})
 
 
 // Create our server listener ----------------------------
